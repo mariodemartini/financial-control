fa437b506773a3176985aa0fe2b0e735065a6c29
mario <mariodemartini@gmail.com>
2026-02-17 18:25:47 -0300

    :sparkles: feat: creating user dtos

---
+++ src/main/java/com/financial_control/dtos/responses/UserResponseDTO.java
@@ -0,0 +1,17 @@
+package com.financial_control.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
