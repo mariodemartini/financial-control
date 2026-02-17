d2f0ce8b3c278d44ccb3a40c47a8c9cfd6d4c4b6
mario <mariodemartini@gmail.com>
2026-02-17 18:25:18 -0300

    :sparkles: feat: creating user entity

---
+++ src/main/java/com/financial_control/entities/UserEntity.java
@@ -0,0 +1,25 @@
+package com.financial_control.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
