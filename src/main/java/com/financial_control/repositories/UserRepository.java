e813480abdab4c7d29c5ced5f20bc4aee23b25d0
mario <mariodemartini@gmail.com>
2026-02-17 18:24:39 -0300

    :sparkles: feat: creating user repository

---
+++ src/main/java/com/financial_control/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.financial_control.repositories;
+
+import com.financial_control.entities.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+}
