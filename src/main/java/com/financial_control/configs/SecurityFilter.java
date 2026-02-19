c509c3d9db23e60f86b1190f3198a9f1698018fb
mario <mariodemartini@gmail.com>
2026-02-19 15:10:01 -0300

    :sparkles: feat: class to implements token

---
+++ src/main/java/com/financial_control/configs/SecurityFilter.java
@@ -0,0 +1,53 @@
+package com.financial_control.configs;
+
+import com.financial_control.repositories.UserRepository;
+import com.financial_control.services.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var token = recoverToken(request);
+
+        if (token != null) {
+            String login = tokenService.validateToken(token);
+
+            UserDetails user = userRepository.findByEmail(login)
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
