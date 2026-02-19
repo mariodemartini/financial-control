914469e0b58dad5182b159efe2c90f5945408d79
mario <mariodemartini@gmail.com>
2026-02-18 21:20:33 -0300

    :sparkles: feat: adding update, delete and searches methods

--- src/main/java/com/financial_control/controllers/UserController.java
+++ src/main/java/com/financial_control/controllers/UserController.java
@@ -4,10 +4,9 @@ import com.financial_control.dtos.requests.UserRequestDTO;
 import com.financial_control.dtos.responses.UserResponseDTO;
 import com.financial_control.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -19,6 +18,26 @@ public class UserController {
     @PostMapping
     public UserResponseDTO createUser(@RequestBody UserRequestDTO userRequestDTO) {
         return userService.createUser(userRequestDTO);
+    }
+
+    @PutMapping("/{id}")
+    public UserResponseDTO updateUser(@PathVariable Long id, @RequestBody UserRequestDTO userRequestDTO) {
+        return userService.updateUser(id, userRequestDTO);
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDTO getUserById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @GetMapping
+    public List<UserResponseDTO> getAllUsers() {
+        return userService.getAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserById(@PathVariable Long id) {
+        userService.deleteById(id);
     }
 
 }
