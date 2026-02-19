82fb9daae850ca155711594c103646a703327a61
mario <mariodemartini@gmail.com>
2026-02-19 11:01:59 -0300

    :sparkles: feat: adding a new search method

--- src/main/java/com/financial_control/repositories/UserRepository.java
+++ src/main/java/com/financial_control/repositories/UserRepository.java
@@ -4,7 +4,11 @@ import com.financial_control.entities.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
 
 }
