caa1053f92ba0f60cf68204d88268ca4fbc9e577
mario <mariodemartini@gmail.com>
2026-02-20 20:35:45 -0300

    :sparkles: feat: adding new method

--- src/main/java/com/financial_control/services/impl/AuthorizationService.java
+++ src/main/java/com/financial_control/services/impl/AuthorizationService.java
@@ -1,8 +1,10 @@
 package com.financial_control.services.impl;
 
+import com.financial_control.entities.UserEntity;
 import com.financial_control.repositories.UserRepository;
 import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,7 +18,23 @@ public class AuthorizationService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não existe"));
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não existe"));
+
+    }
+
+    public UserEntity getAuthenticatedUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String email;
+        if (principal instanceof UserDetails) {
+            email = ((UserDetails)principal).getUsername();
+        } else {
+            email = principal.toString();
+        }
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Usuário logado não encontrado no banco"));
     }
 
 }
