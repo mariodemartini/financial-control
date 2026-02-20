3a8684eb11092bd89263eab29fd65eb7cf1de105
mario <mariodemartini@gmail.com>
2026-02-20 20:37:21 -0300

    :sparkles: feat: creating entity

---
+++ src/main/java/com/financial_control/entities/CategoryEntity.java
@@ -0,0 +1,23 @@
+package com.financial_control.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+}
