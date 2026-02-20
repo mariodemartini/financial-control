5fade461180d9cfb26e71c869d946619f9ca6d34
mario <mariodemartini@gmail.com>
2026-02-20 20:38:49 -0300

    :sparkles: feat: creating DTOs

---
+++ src/main/java/com/financial_control/dtos/requests/CategoryRequestDTO.java
@@ -0,0 +1,14 @@
+package com.financial_control.dtos.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryRequestDTO {
+
+    private String name;
+
+}
---
+++ src/main/java/com/financial_control/dtos/responses/CategoryResponseDTO.java
@@ -0,0 +1,15 @@
+package com.financial_control.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryResponseDTO {
+
+    private Long id;
+    private String name;
+
+}
