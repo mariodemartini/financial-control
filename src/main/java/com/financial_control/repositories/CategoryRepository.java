b45f92a64a6c9f21360e378a2a83b284875b0837
mario <mariodemartini@gmail.com>
2026-02-20 20:36:59 -0300

    :sparkles: feat: creating repository

---
+++ src/main/java/com/financial_control/repositories/CategoryRepository.java
@@ -0,0 +1,17 @@
+package com.financial_control.repositories;
+
+import com.financial_control.entities.CategoryEntity;
+import com.financial_control.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+
+    List<CategoryEntity> findByUser(UserEntity user);
+    Optional<CategoryEntity> findByIdAndUser(Long id, UserEntity user);
+
+}
