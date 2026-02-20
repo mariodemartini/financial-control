99e6c397f9b0a34c50681080764049fac89dfc00
mario <mariodemartini@gmail.com>
2026-02-20 20:33:28 -0300

    :recycle: refactor: changing CrudRepository to JpaRepository

--- src/main/java/com/financial_control/repositories/UserRepository.java
+++ src/main/java/com/financial_control/repositories/UserRepository.java
@@ -1,13 +1,13 @@
 package com.financial_control.repositories;
 
 import com.financial_control.entities.UserEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByEmail(String email);
 
