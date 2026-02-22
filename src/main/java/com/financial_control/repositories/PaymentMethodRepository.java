e51220b33614e1a1e37e54e1eb6ce422481640c4
mario <mariodemartini@gmail.com>
2026-02-22 11:41:04 -0300

    :recycle: refactor: adding new methods to validate user

--- src/main/java/com/financial_control/repositories/PaymentMethodRepository.java
+++ src/main/java/com/financial_control/repositories/PaymentMethodRepository.java
@@ -4,6 +4,13 @@ import com.financial_control.entities.PaymentMethodEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethodEntity, Long> {
+
+    List<PaymentMethodEntity> findByUserId(Long userId);
+    Optional<PaymentMethodEntity> findByIdAndUserId(Long id, Long userId);
+
 }
