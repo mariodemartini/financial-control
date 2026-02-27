4ad653db6c77476426c15e5fff45b3bf1ff2b12b
mario <mariodemartini@gmail.com>
2026-02-26 22:24:50 -0300

    :recycle: refactor: improving createTransaction method and creating auxiliary methods

--- src/main/java/com/financial_control/services/TransactionService.java
+++ src/main/java/com/financial_control/services/TransactionService.java
@@ -1,15 +1,16 @@
 package com.financial_control.services;
 
-import com.financial_control.entities.TransactionEntity;
+import com.financial_control.dtos.requests.TransactionRequestDTO;
+import com.financial_control.dtos.responses.TransactionResponseDTO;
 import com.financial_control.entities.enums.StatusEnum;
 import com.financial_control.entities.enums.TransactionTypeEnum;
 
 public interface TransactionService {
 
-    TransactionEntity createTransaction(TransactionEntity transactionEntity);
-    TransactionEntity updateTransaction(Long id, TransactionEntity transactionEntity);
-    TransactionEntity findTransactionsByMonth(String month);
-    TransactionEntity findTransactionsByType(TransactionTypeEnum transactionType);
-    TransactionEntity findTransactionsByStatus(StatusEnum statusEnum);
+    TransactionResponseDTO createTransaction(TransactionRequestDTO transactionRequestDTO);
+    TransactionResponseDTO updateTransaction(Long id, TransactionRequestDTO transactionRequestDTO);
+    TransactionResponseDTO findTransactionsByMonth(String month);
+    TransactionResponseDTO findTransactionsByType(TransactionTypeEnum transactionType);
+    TransactionResponseDTO findTransactionsByStatus(StatusEnum statusEnum);
 
 }
--- src/main/java/com/financial_control/services/impl/TransactionServiceImpl.java
+++ src/main/java/com/financial_control/services/impl/TransactionServiceImpl.java
@@ -1,13 +1,24 @@
 package com.financial_control.services.impl;
 
+import com.financial_control.dtos.requests.TransactionRequestDTO;
+import com.financial_control.dtos.responses.TransactionResponseDTO;
+import com.financial_control.entities.CardsEntity;
+import com.financial_control.entities.CategoryEntity;
 import com.financial_control.entities.TransactionEntity;
 import com.financial_control.entities.UserEntity;
+import com.financial_control.entities.enums.PaymentTypeEnum;
 import com.financial_control.entities.enums.StatusEnum;
 import com.financial_control.entities.enums.TransactionTypeEnum;
+import com.financial_control.mappers.TransactionMapper;
+import com.financial_control.repositories.CardsRepository;
+import com.financial_control.repositories.CategoryRepository;
 import com.financial_control.repositories.TransactionRepository;
+import com.financial_control.services.CardsService;
 import com.financial_control.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
@@ -18,34 +29,100 @@ public class TransactionServiceImpl implements TransactionService {
     @Autowired
     private AuthorizationService authorizationService;
 
+    @Autowired
+    private TransactionMapper transactionMapper;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private CardsRepository cardsRepository;
+
+    @Autowired
+    private CardsService cardsService;
+
+
     @Override
-    public TransactionEntity createTransaction(TransactionEntity transactionEntity) {
+    public TransactionResponseDTO createTransaction(TransactionRequestDTO transactionRequestDTO) {
+
         UserEntity user = authorizationService.getAuthenticatedUser();
 
-        if (transactionEntity.getTransactionType() == TransactionTypeEnum.REVENUE){
+        CategoryEntity category = categoryRepository.findById(transactionRequestDTO.getCategoryId())
+                .orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
 
+        TransactionEntity transaction = new TransactionEntity();
+        transaction.setDescription(transactionRequestDTO.getDescription());
+        transaction.setAmount(transactionRequestDTO.getAmount());
+        transaction.setTransactionDate(transactionRequestDTO.getTransactionDate());
+        transaction.setStatus(transactionRequestDTO.getStatus());
+        transaction.setCategory(category);
+        transaction.setUser(user);
+        transaction.setTransactionType(transactionRequestDTO.getTransactionType());
+
+        if (transactionRequestDTO.getTransactionType() == TransactionTypeEnum.EXPENSE){
+            CardsEntity card = validateCreditCard(transactionRequestDTO, user);
+
+            transaction.setPaymentType(transactionRequestDTO.getPaymentType());
+            transaction.setCard(card);
+            transaction.setPaymentDate(validatePaymentType(transactionRequestDTO, card));
+
+            transaction.setNumberOfInstallments(1);
+
+            return transactionMapper.toDTO(transactionRepository.save(transaction));
+        
         }
+        else if (transactionRequestDTO.getTransactionType() == TransactionTypeEnum.REVENUE) {
+            transaction.setPaymentDate(transactionRequestDTO.getPaymentDate());
+            transaction.setPaymentType(transactionRequestDTO.getPaymentType());
+            transaction.setNumberOfInstallments(1);
 
+            return transactionMapper.toDTO(transactionRepository.save(transaction));
+        }
+        return null;
+
+    }
+
+    public CardsEntity validateCreditCard(TransactionRequestDTO transactionRequestDTO, UserEntity user) {
+        if (transactionRequestDTO.getPaymentType() == PaymentTypeEnum.CREDIT_CARD){
+            return cardsRepository.findByIdAndUserId(transactionRequestDTO.getCardId(), user.getId())
+                    .orElseThrow(() -> new RuntimeException("Cartão não encontrado"));
+        }
+        return null;
+    }
+
+    public LocalDate validatePaymentType(TransactionRequestDTO transactionRequestDTO, CardsEntity card) {
+
+        if (transactionRequestDTO.getPaymentType() == null){
+            throw new RuntimeException("O tipo de pagamento é obrigatório.");
+        }
+        if (transactionRequestDTO.getPaymentType() == PaymentTypeEnum.CREDIT_CARD){
+            return cardsService.calculatePaymentDate(card, transactionRequestDTO.getTransactionDate());
+
+        } else {
+            return transactionRequestDTO.getPaymentDate();
+        }
+    }
+
+
+
+    @Override
+    public TransactionResponseDTO updateTransaction(Long id, TransactionRequestDTO transactionRequestDTO) {
         return null;
     }
 
     @Override
-    public TransactionEntity updateTransaction(Long id, TransactionEntity transactionEntity) {
+    public TransactionResponseDTO findTransactionsByMonth(String month) {
         return null;
     }
 
     @Override
-    public TransactionEntity findTransactionsByMonth(String month) {
+    public TransactionResponseDTO findTransactionsByType(TransactionTypeEnum transactionType) {
         return null;
     }
 
     @Override
-    public TransactionEntity findTransactionsByType(TransactionTypeEnum transactionType) {
+    public TransactionResponseDTO findTransactionsByStatus(StatusEnum statusEnum) {
         return null;
     }
 
-    @Override
-    public TransactionEntity findTransactionsByStatus(StatusEnum statusEnum) {
-        return null;
-    }
 }
